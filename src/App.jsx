e491ad304feb33f7ac039fde175d83a43a9686fb
Shihab Uddin <shihabuddin.dev@gmail.com>
2025-04-05 20:03:05 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className='text-red-500'>Hello</h1>
+    <button className='btn btn-primary'>b</button>
+    </>
+  )
+}
+
+export default App
